c7152fd0dc5a33793078399ea38910e4d885d12a
Oussama BOUHLEL <oussama_bouhlel@outlook.fr>
2025-05-09 15:40:08 +0200

    Update the context for cuddle

--- src/commands/cuddle.ts
+++ src/commands/cuddle.ts
@@ -1,11 +1,15 @@
 import type { ChatInputCommandInteraction } from "discord.js";
-import { SlashCommandBuilder } from "discord.js";
+import { InteractionContextType, SlashCommandBuilder } from "discord.js";
 import axios from "axios";
 
 export const cuddle = {
   data: new SlashCommandBuilder()
     .setName("cuddle")
-    .setDescription("Send a cuddle"),
+    .setDescription("Send a cuddle")
+    .setContexts([
+      InteractionContextType.PrivateChannel,
+      InteractionContextType.Guild,
+    ]),
   async execute(interaction: ChatInputCommandInteraction) {
     try {
       const { data } = await axios.get("https://api.waifu.pics/sfw/cuddle");
