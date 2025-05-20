772667cb7d8a559849a1e846101f5c53f63040be
Oussama BOUHLEL <oussama_bouhlel@outlook.fr>
2025-05-20 18:27:44 +0200

    Update the command api

--- src/controllers/commands.ts
+++ src/controllers/commands.ts
@@ -8,7 +8,7 @@ export async function getCommands(
   try {
     const commands = discord.getCommandData();
     await reply.status(200).send({
-      commands: commands,
+      commands,
     });
   } catch (error) {
     console.error(error);
