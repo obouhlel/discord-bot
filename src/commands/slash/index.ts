25cab70316808fb4f5f732609985ee587e7de31f
Oussama BOUHLEL <oussama_bouhlel@outlook.fr>
2025-05-21 16:02:17 +0200

    Remove quoi feur

--- src/commands/slash/index.ts
+++ src/commands/slash/index.ts
@@ -2,5 +2,4 @@ export { ping } from "./ping";
 export { echo } from "./echo";
 export { cuddle } from "./cuddle";
 export { welcome } from "./welcome";
-export { score } from "./score";
 export { llm } from "./llm";
