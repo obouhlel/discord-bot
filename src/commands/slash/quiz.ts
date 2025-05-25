eeacc0cdf120a9d99b4aeba872825284f16b188e
Oussama BOUHLEL <oussama_bouhlel@outlook.fr>
2025-05-25 16:06:51 +0200

    Reset timeouts

--- src/commands/slash/quiz.ts
+++ src/commands/slash/quiz.ts
@@ -56,6 +56,18 @@ export const quiz = {
 
     const key = `quiz:${guild.id}:${channel.id}`;
 
+    // Reset Timeout
+    const one = timeouts.get(key + ":one");
+    if (one) {
+      clearTimeout(one);
+      timeouts.delete(key + ":one");
+    }
+    const end = timeouts.get(key + ":end");
+    if (end) {
+      clearTimeout(end);
+      timeouts.delete(key + ":end");
+    }
+
     await interaction.deferReply();
 
     if (await alreadyRunning(redis, key)) {
