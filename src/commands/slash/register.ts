a5d0d86d5ff5ad3ae59a8307fec82530161d4777
Oussama BOUHLEL <114084470+obouhlel@users.noreply.github.com>
2025-06-08 13:08:31 +0200

    Register the user (#20)

--- src/commands/slash/register.ts
+++ src/commands/slash/register.ts
@@ -1,19 +1,15 @@
 import type {
-  ButtonInteraction,
   ChatInputCommandInteraction,
   ColorResolvable,
-  DMChannel,
-  Message,
+  TextChannel,
 } from "discord.js";
 import {
-  ActionRowBuilder,
   ApplicationIntegrationType,
-  ButtonBuilder,
-  ButtonStyle,
   EmbedBuilder,
   InteractionContextType,
   SlashCommandBuilder,
 } from "discord.js";
+import type { PrismaClient } from "generated/prisma";
 import AnilistService from "services/anilist";
 import type CustomDiscordClient from "types/custom-discord-client";
 import { capitalize } from "utils/capitalize";
@@ -22,14 +18,33 @@ export const register = {
   data: new SlashCommandBuilder()
     .setName("register")
     .setDescription("Register the anilist or mal")
+    .addStringOption((option) =>
+      option
+        .setName("service")
+        .setDescription("Choose your anime list service")
+        .setRequired(true)
+        .addChoices(
+          { name: "Anilist", value: "anilist" },
+          { name: "MyAnimeList", value: "mal" },
+        ),
+    )
+    .addStringOption((option) =>
+      option
+        .setName("username")
+        .setDescription("The username of service")
+        .setRequired(true),
+    )
     .setIntegrationTypes([
       ApplicationIntegrationType.GuildInstall,
       ApplicationIntegrationType.UserInstall,
     ])
-    .setContexts([InteractionContextType.BotDM]),
+    .setContexts([InteractionContextType.BotDM, InteractionContextType.Guild]),
 
   async execute(interaction: ChatInputCommandInteraction) {
     const client = interaction.client as CustomDiscordClient;
+    const service = interaction.options.getString("service", true);
+    const username = interaction.options.getString("username", true);
+    const channel = interaction.channel as TextChannel;
     const { prisma } = client;
     const user = interaction.user;
 
@@ -38,26 +53,9 @@ export const register = {
       return;
     }
 
-    const embed = new EmbedBuilder()
-      .setTitle("Register")
-      .setColor("Blue")
-      .setDescription("Chose between Anilist or MyAnimeList");
+    await interaction.reply("Register the user...");
 
-    const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
-      new ButtonBuilder()
-        .setCustomId("register_anilist")
-        .setLabel("Anilist")
-        .setStyle(ButtonStyle.Primary),
-      new ButtonBuilder()
-        .setCustomId("register_mal")
-        .setLabel("MyAnimeList")
-        .setStyle(ButtonStyle.Primary)
-        .setDisabled(true),
-    );
-
-    await interaction.reply({ embeds: [embed], components: [row] });
-
-    await prisma.user.upsert({
+    const dbUser = await prisma.user.upsert({
       where: {
         discordId: user.id,
       },
@@ -72,112 +70,84 @@ export const register = {
         username: user.username,
         avatarId: user.avatar,
       },
+      include: {
+        animeListUser: true,
+      },
     });
-  },
 
-  async anilist(interaction: ButtonInteraction) {
-    const channel = interaction.channel as DMChannel | null;
-    const user = interaction.user;
-    const { prisma } = interaction.client as CustomDiscordClient;
-    if (!channel) {
-      await interaction.reply("Please use it in the dm of bot channel");
+    if (service === "anilist") {
+      await this.anilist(username, channel, dbUser.id, prisma);
       return;
     }
-    await interaction.reply("Please send your Anilist username");
+  },
 
-    const filter = (message: Message) =>
-      message.author.id === interaction.user.id;
-    const collector = channel.createMessageCollector({
-      filter,
-      max: 1,
-      time: 15_000,
-    });
-
-    // eslint-disable-next-line
-    collector.on("end", async (collected) => {
-      if (collected.size === 0) {
-        await channel.send("Time out - No response received");
-        return;
-      }
-      const username = collected.first()?.content;
-      if (!username) {
-        await channel.send("Please send only the username");
-        return;
-      }
-      const anilist = new AnilistService();
-      const anilistUser = await anilist.getUser(username);
-      if (!anilistUser) {
-        const embed = new EmbedBuilder()
-          .setTitle("Error")
-          .setColor("Red")
-          .setDescription("User not found");
-        await channel.send({ embeds: [embed] });
-        return;
-      }
-
-      const dbUser = await prisma.user.findUnique({
-        where: {
-          discordId: user.id,
-        },
-      });
-      if (!dbUser) {
-        await channel.send("Please retry the command `/register`");
-        return;
-      }
-
-      const animes = await anilist.getMalIds(anilistUser.id, anilistUser.name);
-      if (!animes) {
-        await channel.send("Please retry the command `/register`");
-        return;
-      }
-
-      await prisma.animeListUser.upsert({
-        where: {
-          userId: dbUser.id,
-        },
-        create: {
-          type: "ANILIST",
-          typeId: anilistUser.id,
-          username: anilistUser.name,
-          status: ["CURRENT", "COMPLETED", "DROPPED", "PAUSED", "PLANNING"],
-          animes: {
-            createMany: {
-              data: animes,
-            },
-          },
-          userId: dbUser.id,
-        },
-        update: {
-          type: "ANILIST",
-          typeId: anilistUser.id,
-          username: anilistUser.name,
-          animes: {
-            deleteMany: {},
-            createMany: {
-              data: animes,
-            },
-          },
-          userId: dbUser.id,
-        },
-      });
-
-      const nbAnimes = animes
-        .flatMap((status) => status.malId)
-        .flat()
-        .length.toString();
-
+  async anilist(
+    username: string,
+    channel: TextChannel,
+    dbUserId: number,
+    prisma: PrismaClient,
+  ) {
+    const anilist = new AnilistService();
+    const anilistUser = await anilist.getUser(username);
+    if (!anilistUser) {
       const embed = new EmbedBuilder()
-        .setTitle(capitalize(anilistUser.name))
-        .setImage(anilistUser.avatar.large)
-        .setColor(
-          capitalize(anilistUser.options.profileColor) as ColorResolvable,
-        )
-        .setDescription(
-          `User found with ${nbAnimes}, if you want to sort please type \`/filter\``,
-        )
-        .setURL(anilistUser.siteUrl);
-
+        .setTitle("Error")
+        .setColor("Red")
+        .setDescription("User not found");
       await channel.send({ embeds: [embed] });
+      return;
+    }
+
+    const animes = await anilist.getMalIds(anilistUser.id, anilistUser.name);
+    if (!animes) {
+      await channel.send("Please retry the command `/register`");
+      return;
+    }
+
+    await prisma.animeListUser.upsert({
+      where: {
+        userId: dbUserId,
+      },
+      create: {
+        type: "ANILIST",
+        typeId: anilistUser.id,
+        username: anilistUser.name,
+        status: ["CURRENT", "COMPLETED", "DROPPED", "PAUSED", "PLANNING"],
+        animes: {
+          createMany: {
+            data: animes,
+          },
+        },
+        userId: dbUserId,
+      },
+      update: {
+        type: "ANILIST",
+        typeId: anilistUser.id,
+        username: anilistUser.name,
+        animes: {
+          deleteMany: {},
+          createMany: {
+            data: animes,
+          },
+        },
+        userId: dbUserId,
+      },
     });
+
+    const nbAnimes = animes
+      .flatMap((status) => status.malId)
+      .flat()
+      .length.toString();
+
+    const embed = new EmbedBuilder()
+      .setTitle(capitalize(anilistUser.name))
+      .setImage(anilistUser.avatar.large)
+      .setColor(capitalize(anilistUser.options.profileColor) as ColorResolvable)
+      .setDescription(
+        `User found with ${nbAnimes}, if you want to sort please type \`/filter\``,
+      )
+      .setURL(anilistUser.siteUrl);
+
+    await channel.send({ embeds: [embed] });
   },
 };
